3a7e56c2a030e2545d95daacc6176f153555dc00
ElwinBran <elwinbran@gmail.com>
2020-12-30 17:03:30 +0100

    Mobile header and footer changes

--- footer_script.js
+++ footer_script.js
@@ -1,1 +1,8 @@
 //initial
+document.getElementsByClassName("container page-container")[0].classList.add("mobile-padding-zero");
+let mainNav = document.getElementsByClassName("row main-navigation")[0];
+mainNav.classList.add("mobile-padding-zero");
+mainNav.firstElementChild.classList.add("mobile-padding-zero");
+document.getElementById("content").classList.add("mobile-padding-zero");
+document.getElementById("sidebar-left").classList.add("mobile-padding-zero");
+document.getElementsByClassName("site-footer")[0].classList.add("mobile-add-margin");
