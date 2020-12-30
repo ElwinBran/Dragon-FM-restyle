b6d9c67c12e7c9c72291027d66dcbfe7f2753340
ElwinBran <elwinbran@gmail.com>
2020-12-30 19:02:44 +0100

    Major change renaming the CSS file since it will be hardcoded to the JS. Now it only serves as reference. No further changes

--- footer_script.js
+++ footer_script.js
@@ -1,3 +1,20 @@
+// Add style hardcoded
+ $('head').append('<style type="text/css">' +
+ '@media(max-width:768px){.mobile-padding-zero {padding: 0;}' +
+    '.mobile-add-margin {' +
+    'margin-left: 8px;' +
+    'margin-right: 8px;' +
+    '}' +
+    '.mobile-remove-rounding {' +
+    'border-top-left-radius: 0px;' +
+        'border-top-right-radius: 0px;' +
+        'border-bottom-left-radius: 0px;' +
+        'border-bottom-right-radius: 0px;' +
+        '}' +
+ '}' +
+ '</style>');
+
+// Start applying new styling
 document.getElementsByClassName("container page-container")[0].classList.add("mobile-padding-zero");
 let mainNav = document.getElementsByClassName("row main-navigation")[0];
 mainNav.classList.add("mobile-padding-zero");
