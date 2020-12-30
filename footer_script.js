6f35c1d58b481d7cf1dd65fc6b9423cfdf059b7d
ElwinBran <elwinbran@gmail.com>
2020-12-30 17:32:27 +0100

    Redid mobile footer to appear as a bar like the top

--- footer_script.js
+++ footer_script.js
@@ -1,8 +1,7 @@
-//initial
 document.getElementsByClassName("container page-container")[0].classList.add("mobile-padding-zero");
 let mainNav = document.getElementsByClassName("row main-navigation")[0];
 mainNav.classList.add("mobile-padding-zero");
 mainNav.firstElementChild.classList.add("mobile-padding-zero");
 document.getElementById("content").classList.add("mobile-padding-zero");
 document.getElementById("sidebar-left").classList.add("mobile-padding-zero");
-document.getElementsByClassName("site-footer")[0].classList.add("mobile-add-margin");
+document.getElementsByClassName("site-footer")[0].classList.add("mobile-remove-rounding");
