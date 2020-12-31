5755cb38bc5866549f51430a22d8dc2eef653c70
ElwinBran <elwinbran@gmail.com>
2020-12-31 09:50:56 +0100

    Added code to remove footer empty space

--- footer_script.js
+++ footer_script.js
@@ -1,18 +1,5 @@
 // Add style hardcoded
- $('head').append('<style type="text/css">' +
- '@media(max-width:768px){.mobile-padding-zero {padding: 0;}' +
-    '.mobile-add-margin {' +
-    'margin-left: 8px;' +
-    'margin-right: 8px;' +
-    '}' +
-    '.mobile-remove-rounding {' +
-    'border-top-left-radius: 0px;' +
-        'border-top-right-radius: 0px;' +
-        'border-bottom-left-radius: 0px;' +
-        'border-bottom-right-radius: 0px;' +
-        '}' +
- '}' +
- '</style>');
+ $('head').append('<style type="text/css">@media(max-width:768px){.mobile-padding-zero{padding:0}.mobile-add-margin{margin-left:8px;margin-right:8px}.mobile-remove-rounding{border-top-left-radius:0;border-top-right-radius:0;border-bottom-left-radius:0;border-bottom-right-radius:0}}.vertical-padding--none{padding-top:0;padding-bottom:0}</style>');
 
 // Start applying new styling
 document.getElementsByClassName("container page-container")[0].classList.add("mobile-padding-zero");
@@ -22,3 +9,9 @@ mainNav.firstElementChild.classList.add("mobile-padding-zero");
 document.getElementById("content").classList.add("mobile-padding-zero");
 document.getElementById("sidebar-left").classList.add("mobile-padding-zero");
 document.getElementsByClassName("site-footer")[0].classList.add("mobile-remove-rounding");
+
+// Remove large footer empty space after loading. (this code is ran in the footer)
+$('#footer-row').load(function() {
+	$('#footer-row').find("br").remove();
+    $('#footer-row').find(".widget_header_met_opmaak_content_1").eq(1).addClass("vertical-padding--none");
+});
