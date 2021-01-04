1f0aa49d3d492c4384aae56596356cbc34a7cc1c
ElwinBran <elwinbran@gmail.com>
2021-01-04 17:15:20 +0100

    Completely redid the system for smoother override and better configurability. This file must now not use '$' anymore to prevent errors

--- footer_script.js
+++ footer_script.js
@@ -1,27 +1,54 @@
-// Add style hardcoded
- $('head').append('<style type="text/css">@media(max-width:768px){.small--no-padding{padding:0}.small--no-rounding{border-radius:0}.small--no-bottom-margin{margin-bottom:0}}@media(min-width:992px){.from-medium--default-rounding{border-radius:10px}}.remove-vertical-padding{padding-top:0;padding-bottom:0}</style>');
+// Add styling hardcoded
+jQuery("head").append('<style type="text/css">@media(max-width:768px){.small--no-padding{padding:0}.small--no-rounding{border-radius:0}.small--no-bottom-margin{margin-bottom:0}}@media(min-width:992px){.from-medium--default-rounding{border-radius:10px}}.remove-vertical-padding{padding-top:0;padding-bottom:0}</style>');
 
-// Start applying new styling
-$(".container.page-container").addClass("small--no-padding");
-let mainNav = $(".row.main-navigation").first();
-mainNav.addClass("small--no-padding");
-mainNav.children().first().addClass("small--no-padding");
-$("#content").addClass("small--no-padding");
-$("#sidebar-left").addClass("small--no-padding");
-$(".site-footer").addClass("small--no-rounding");
+function recenterChatButton(styleVariables) {
+    //here should be code to handle extra overrides. Mappings must be exported in the API.
+    let buttonContainer = jQuery(".widget_default_blok_2").find(".widget_button");
+    buttonContainer.css("background-size", "cover");
+    buttonContainer.find(".widget_button_tekst").css("width", "288px");
+}
+function addPlayerRounding(styleVariables) {
+    jQuery("#sidebar-left").children(".widget_default_blok_1").addClass("from-medium--default-rounding");
+}
+function removeMobileBodyPadding() {
+    jQuery("body").addClass("small--no-bottom-margin");
+}
+function removeMobileContentPadding() {
+    jQuery(".container.page-container").first().addClass("small--no-padding");
+    jQuery("#content").addClass("small--no-padding");
+}
+function removeMobileHeaderPadding() {
+    let mainNav = jQuery(".row.main-navigation").first();
+    mainNav.addClass("small--no-padding");
+    mainNav.children().first().addClass("small--no-padding");
+}
+function removeMobilePlayerPadding() {
+    jQuery("#sidebar-left").addClass("small--no-padding");
+}
+function removeMobileFooterPadding() {
+    jQuery("body").ready(function() {
+        jQuery(".footer_compleet_onder").addClass("small--no-bottom-margin");
+    });
+    jQuery("#footer-row").ready(function() {
+        jQuery("#footer-row").find("br").remove();
+        jQuery("#footer-row").find(".widget_header_met_opmaak_content_1").eq(1).addClass("remove-vertical-padding");
+    });
+}
+function removeMobileFooterRounding() {
+    jQuery(".site-footer").addClass("small--no-rounding");
+}
 
-let buttonContainer = $(".widget_default_blok_2").find(".widget_button");
-buttonContainer.css("background-size", "cover");
-buttonContainer.find(".widget_button_tekst").css("width", "288px");
+var latestBody = [removeMobileBodyPadding];
+var latestHeader = [removeMobileHeaderPadding];
+var latestContent = [removeMobileContentPadding];
+var latestPlayer = [removeMobilePlayerPadding, recenterChatButton, addPlayerRounding];
+var latestFooter = [removeMobileFooterPadding, removeMobileFooterRounding];
 
-$("#footer-row").ready(function() {
-	$("#footer-row").find("br").remove();
-    $("#footer-row").find(".widget_header_met_opmaak_content_1").eq(1).addClass("remove-vertical-padding");
-});
-
-$("body").ready(function() {
-	$("body").addClass("small--no-bottom-margin");
-    $(".footer_compleet_onder").addClass("small--no-bottom-margin");
-});
-
-$("#sidebar-left").children(".widget_default_blok_1").addClass("from-medium--default-rounding");
+//
+// tasks: a list of functions to perform
+// OPTIONAL | styleVariables: a map {string, string} of a propery to be overridden 
+function override(tasks, styleVariables) {
+    jQuery(tasks).each(function(key, val){
+        val(styleVariables);
+    });
+}
