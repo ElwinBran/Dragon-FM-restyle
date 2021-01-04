56b8b7e87b652cc18355d5fe23c74a2169c20b67
ElwinBran <elwinbran@gmail.com>
2021-01-04 15:44:06 +0100

    Improved CSS naming, uses jQuery exclusively, removed unused CSS class

--- footer_script.js
+++ footer_script.js
@@ -1,14 +1,14 @@
 // Add style hardcoded
- $('head').append('<style type="text/css">@media(max-width:768px){.mobile-padding-zero{padding:0}.mobile-add-margin{margin-left:8px;margin-right:8px}.mobile-remove-rounding{border-top-left-radius:0;border-top-right-radius:0;border-bottom-left-radius:0;border-bottom-right-radius:0}.mobile-bottom-padding--none{margin-bottom:0}}@media(min-width:992px){.no-handheld--rounding{border-radius:10px}}.vertical-padding--none{padding-top:0;padding-bottom:0}</style>');
+ $('head').append('<style type="text/css">@media(max-width:768px){.small--no-padding{padding:0}.small--no-rounding{border-radius:0}.small--no-bottom-margin{margin-bottom:0}}@media(min-width:992px){.from-medium--default-rounding{border-radius:10px}}.remove-vertical-padding{padding-top:0;padding-bottom:0}</style>');
 
 // Start applying new styling
-document.getElementsByClassName("container page-container")[0].classList.add("mobile-padding-zero");
-let mainNav = document.getElementsByClassName("row main-navigation")[0];
-mainNav.classList.add("mobile-padding-zero");
-mainNav.firstElementChild.classList.add("mobile-padding-zero");
-document.getElementById("content").classList.add("mobile-padding-zero");
-document.getElementById("sidebar-left").classList.add("mobile-padding-zero");
-document.getElementsByClassName("site-footer")[0].classList.add("mobile-remove-rounding");
+$(".container.page-container").addClass("small--no-padding");
+let mainNav = $(".row.main-navigation").first();
+mainNav.classList.add("small--no-padding");
+mainNav.firstElementChild.classList.add("small--no-padding");
+$("#content").addClass("small--no-padding");
+$("#sidebar-left").addClass("small--no-padding");
+$(".site-footer").addClass("small--no-rounding");
 
 let buttonContainer = $(".widget_default_blok_2").find(".widget_button");
 buttonContainer.css("background-size", "cover");
@@ -16,12 +16,12 @@ buttonContainer.find(".widget_button_tekst").css("width", "288px");
 
 $("#footer-row").ready(function() {
 	$("#footer-row").find("br").remove();
-    $("#footer-row").find(".widget_header_met_opmaak_content_1").eq(1).addClass("vertical-padding--none");
+    $("#footer-row").find(".widget_header_met_opmaak_content_1").eq(1).addClass("remove-vertical-padding");
 });
 
 $("body").ready(function() {
-	$("body").addClass("mobile-bottom-padding--none");
-    $(".footer_compleet_onder").addClass("mobile-bottom-padding--none");
+	$("body").addClass("small--no-bottom-margin");
+    $(".footer_compleet_onder").addClass("small--no-bottom-margin");
 });
 
-$("#sidebar-left").children(".widget_default_blok_1").addClass("no-handheld--rounding");
+$("#sidebar-left").children(".widget_default_blok_1").addClass("from-medium--default-rounding");
