31154b4a8848425a80e1a4a32fcbdf0594d845a2
ElwinBran <elwinbran@gmail.com>
2021-04-27 08:51:30 +0200

    orange update

--- footer_script.js
+++ footer_script.js
@@ -38,7 +38,7 @@ function removeMobileFooterRounding() {
     jQuery(".site-footer").addClass("small--no-rounding");
 }
 function fullSizeHeaderBanner(styleVariables) {
-    let backVar = "#57007F"; // Default
+    let backVar = "#ff6e00"; // Default
     if(!(styleVariables === undefined)) {
         if(styleVariables.size != 0) {
             let bannerBackgroundOverride = "";
